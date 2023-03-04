97375cefcb8600b16375db307ba63839e311c040
tunahan <tunahancelebi@derinbilgi.com.tr>
2023-03-04 20:50:38 +0300

    some menu fixes

--- src/Admin/Layout/SidebarLayout.js
+++ src/Admin/Layout/SidebarLayout.js
@@ -1,5 +1,7 @@
-//import { useState } from 'react';
-import { Button, Menu } from 'antd';
+import { useState } from 'react'
+import { Button, Menu } from 'antd'
+import { NavigateFunction, useNavigate } from 'react-router-dom'
+
 import {
 	AppstoreOutlined,
 	ContainerOutlined,
@@ -23,7 +25,7 @@ const items = [
 	getItem('Dashboard', '2', <AppstoreOutlined />),
 	getItem('Dashboard', '3', <PieChartOutlined />),
 	getItem('Ürün Yönetimi', 'sub1', <ContainerOutlined />, [
-		getItem('Ürünler', '5'),
+		getItem('Ürünler', 'products/'),
 		getItem('Option 6', '6'),
 		getItem('Option 7', '7'),
 		getItem('Option 8', '8'),
@@ -31,17 +33,27 @@ const items = [
 	getItem('Sipariş Yönetimi', 'sub2', <ContainerOutlined />, [
 		getItem('Option 9', '9'),
 		getItem('Option 10', '10'),
-		getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
+		getItem('Submenu', 'sub3', null, [
+			getItem('Option 11', '11'), 
+			getItem('Option 12', '12')
+		]),
 	]),
-	getItem('Üye Yönetimi', 'sub2', <ContainerOutlined />, [
-		getItem('Option 9', '9'),
-		getItem('Option 10', '10'),
-		getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
+	getItem('Üye Yönetimi', 'sub4', <ContainerOutlined />, [
+		getItem('Option 90', '90'),
+		getItem('Option 100', '100'),
+		getItem('Submenu', 'sub5', null, [
+			getItem('Option 20', '20'), 
+			getItem('Option 21', '21')
+		]),
 	]),
+	getItem('Çıkış', 'logout/', <PieChartOutlined />),
 ];
 
 
 const SidebarLayout = () => {
+	const [selectedMenuItem, setSelectedMenuItem]= useState('');
+	const navigate = useNavigate()
+
 	return (
 		<div>
 			<div className="logo"/>
@@ -49,6 +61,8 @@ const SidebarLayout = () => {
 				defaultOpenKeys={['sub1']}
 				mode="inline"
 				theme="light"
+				onClick={(e) => 
+					setSelectedMenuItem(e.key)}
 				items={items} />
 		</div>
   )
