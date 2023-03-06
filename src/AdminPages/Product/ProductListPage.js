b60d16de7e086fb1beb839803da9ed0972ecc0a5
tunahan <tunahancelebi@derinbilgi.com.tr>
2023-03-07 02:53:27 +0300

    pagination added

--- src/AdminPages/Product/ProductListPage.js
+++ src/AdminPages/Product/ProductListPage.js
@@ -5,6 +5,14 @@ import { useNavigate } from 'react-router-dom'
 
 const columns = [
    {
+      title: '#',
+      dataIndex: 'id',
+      key: 'id',
+      render: (text, record) => (
+         <a href={record.id + "/edit"}>{text}</a>
+      ),
+   },
+   {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
@@ -17,14 +25,85 @@ const columns = [
    },
    {
       title: 'Created Date',
-      dataIndex: 'created_date',
-      key: 'created_date',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
    },
    {
       title: 'Updated Date',
-      dataIndex: 'updated_date',
-      key: 'updated_date',
+      dataIndex: 'updatedAt',
+      key: 'updatedAt',
    },
+   {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+         <Space size="middle">
+         <a>Edit</a>
+         <a>Delete</a>
+         </Space>
+      ),
+   }
+ ];
+ var data = [
+   {
+      key: '3',
+      id: 4,
+      name: 'Joe Black',
+      stock: 32,
+      updated_date: 'Sydney No. 1 Lake Park',
+      created_date: 'New York No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+   },
+];
+
+
+function ProductListPage() {
+   const navigate = useNavigate()
+   const [productList, setproductList] = useState(data);
+   const [totalCount, settotalCount] = useState(5);
+   const [page, setpage] = useState(0);
+   const [pageSize, setpageSize] = useState(5);
+
+   useEffect(() => {
+      const url = new URL("http://localhost:5268/api/products")
+      const params = { page:page, pageSize:pageSize }
+      url.search = new URLSearchParams(params).toString();
+
+      const requestOptions = {
+         method: 'GET',
+         headers: { 'Content-Type': 'application/json' },
+      };
+      fetch(url, requestOptions)
+         .then(response => response.json())
+         .then(datas => {
+            setproductList(datas.products)
+            settotalCount(datas.totalCount)
+         });
+   }, [pageSize, page]);
+
+   const onChange = (pageNumber, pageSize) => {
+      //pagination için
+      setpage(pageNumber - 1)
+      setpageSize(pageSize)
+      console.log('Page: ', pageNumber);
+      console.log('Page: ', pageSize);
+    };
+
+
+   return (
+      <div>
+         <div className='d-flex justify-content-end'>
+            <Button onClick={() => {navigate(`/admin/products/add/`)}} className='m-2 me-2' type="primary" ghost>Ürün Ekle +</Button>
+         </div>
+         <Table columns={columns} dataSource={productList} rowKey={(record) => record.id}
+            pagination={{ defaultPageSize: pageSize, showSizeChanger: true, pageSizeOptions: ['10', '25', '50'], total:totalCount, onChange:onChange }}
+         />
+      </div>
+   )
+}
+
+export default ProductListPage
+
    // {
    //    title: 'Tags',
    //    key: 'tags',
@@ -45,70 +124,3 @@ const columns = [
    //       </>
    //    ),
    // },
-   {
-      title: 'Action',
-      key: 'action',
-      render: (_, record) => (
-         <Space size="middle">
-         <a>Edit</a>
-         <a>Delete</a>
-         </Space>
-      ),
-   },
- ];
- var data = [
-   {
-      key: '1',
-      name: 'John Brown',
-      stock: 32,
-      updated_date: 'New York No. 1 Lake Park',
-      created_date: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-   },
-   {
-      key: '2',
-      name: 'Jim Green',
-      stock: 42,
-      updated_date: 'London No. 1 Lake Park',
-      created_date: 'New York No. 1 Lake Park',
-      tags: ['loser'],
-   },
-   {
-      key: '3',
-      id: 4,
-      name: 'Joe Black',
-      stock: 32,
-      updated_date: 'Sydney No. 1 Lake Park',
-      created_date: 'New York No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-   },
- ];
-
-function ProductListPage() {
-   const navigate = useNavigate()
-   const [productList, setproductList] = useState(data);
-
-   useEffect(() => {
-      var a = [];
-      const requestOptions = {
-         method: 'GET',
-         headers: { 'Content-Type': 'application/json' },
-      };
-      fetch('http://localhost:5268/api/products', requestOptions)
-         .then(response => response.json())
-         .then(datas => setproductList(datas));
-   }, []);
-
-   return (
-      <div>
-         <div className='d-flex justify-content-end'>
-            <Button onClick={() => {navigate(`/admin/products/add/`)}} className='m-2 me-2' type="primary" ghost>Ürün Ekle +</Button>
-         </div>
-         <Table columns={columns} dataSource={productList} 
-            pagination={{ defaultPageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '20', '30']}}
-         />
-      </div>
-   )
-}
-
-export default ProductListPage
